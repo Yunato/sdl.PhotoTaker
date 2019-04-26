697e2724962db28160b32450d78647e952a91dbf
Yunato <yunato525@gmail.com>
2019-04-26 16:57:39 +0900

    Add function calling for camera application

--- app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
@@ -3,8 +3,9 @@ package jp.ac.titech.itpro.sdl.phototaker;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -26,9 +27,7 @@ public class MainActivity extends AppCompatActivity {
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                // TODO: You should setup appropriate parameters for the intent
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
@@ -54,7 +53,9 @@ public class MainActivity extends AppCompatActivity {
         switch (reqCode) {
             case REQ_PHOTO:
                 if (resCode == RESULT_OK) {
-                    // TODO: You should implement the code that retrieve a bitmap image
+                    Bundle extras = data.getExtras();
+                    Bitmap bitmap = (Bitmap)extras.get("data");
+                    ((ImageView)findViewById(R.id.photo_view)).setImageBitmap(bitmap);
                 }
                 break;
         }
